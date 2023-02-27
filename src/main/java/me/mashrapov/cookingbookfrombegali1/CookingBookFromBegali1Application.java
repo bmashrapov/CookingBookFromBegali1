b62619f2dd93e34bf07d230c5192075851be9be0
b.Mashrapov <b_mashrapov@mail.ru>
2023-02-27 08:43:00 +0100

    Initial commit

---
+++ src/main/java/me/mashrapov/cookingbookfrombegali1/CookingBookFromBegali1Application.java
@@ -0,0 +1,13 @@
+package me.mashrapov.cookingbookfrombegali1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CookingBookFromBegali1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CookingBookFromBegali1Application.class, args);
+    }
+
+}
---
+++ src/test/java/me/mashrapov/cookingbookfrombegali1/CookingBookFromBegali1ApplicationTests.java
@@ -0,0 +1,13 @@
+package me.mashrapov.cookingbookfrombegali1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CookingBookFromBegali1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
